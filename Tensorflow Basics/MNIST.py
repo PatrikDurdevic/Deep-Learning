e2367dde4df1c1b9bae98ec205b71b1c7972840e
PatrikDurdevic <patrik.durdevic@gmail.com>
2020-04-07 09:04:23 +0200

    Create MNIST.py

---
+++ Tensorflow Basics/MNIST.py
@@ -0,0 +1,98 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+
+train_images = train_images / 255
+test_images = test_images / 255
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10)
+])
+model.compile(optimizer='adam',
+				loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+				metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
+
+plt.figure(figsize=[8,6])
+plt.plot(history.history['accuracy'],'r',linewidth=3.0)
+plt.plot(history.history['val_accuracy'],'b',linewidth=3.0)
+plt.legend(['Training Accuracy', 'Validation Accuracy'],fontsize=18)
+plt.xlabel('Epochs ',fontsize=16)
+plt.ylabel('Accuracy',fontsize=16)
+plt.title('Accuracy Curves',fontsize=16)
+plt.show()
+
+
+def plot_image(i, predictions_array, true_label, img):
+	predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+	plt.grid(False)
+	plt.xticks([])
+	plt.yticks([])
+
+	plt.imshow(img, cmap=plt.cm.binary)
+
+	predicted_label = np.argmax(predictions_array)
+	if predicted_label == true_label:
+		color = 'blue'
+	else:
+		color = 'red'
+
+	plt.xlabel("{} {:2.0f}% ({})".format(predicted_label,
+										100*np.max(predictions_array),
+										true_label),
+										color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+	predictions_array, true_label = predictions_array, true_label[i]
+	plt.grid(False)
+	plt.xticks(range(10))
+	plt.yticks([])
+	thisplot = plt.bar(range(10), predictions_array, color="#777777")
+	plt.ylim([0, 1])
+	predicted_label = np.argmax(predictions_array)
+
+	thisplot[predicted_label].set_color('red')
+	thisplot[true_label].set_color('blue')
+
+
+
+probability_model = tf.keras.Sequential([model, 
+										tf.keras.layers.Softmax()])
+predictions = probability_model.predict(test_images)
+
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+	plt.subplot(num_rows, 2*num_cols, 2*i+1)
+	plot_image(i, predictions[i], test_labels, test_images)
+	plt.subplot(num_rows, 2*num_cols, 2*i+2)
+	plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
